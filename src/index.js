4fb4e300f1e8675416fdda9076111e49db301191
ipapasa <ioannis@papasavvas.net>
2017-01-25 17:49:05 +0000

    JS environment without CI and production

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+
+//import numeral from "numeral";
+
+//const value = numeral(1000).format("$0,0.00");
+//debugger;
+//console.log(`I would pay ${value} for this awesome product!`);
+
