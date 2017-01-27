3551dae5f78ef8cc8d16be6d9efe7acc9e757ae9
ipapasa <ioannis@papasavvas.net>
2017-01-27 22:03:03 +0000

    Http calls, Fetch, Mock Data

--- src/index.test.js
+++ src/index.test.js
@@ -8,17 +8,30 @@ describe("Our first test", () => {
   });
 });
 
-describe("index.html", () => {
-  it("should say hello", (done) => {
-    const index = fs.readFileSync("./src/index.html", "utf-8");
-    jsdom.env(index, function (err, window) {
-      // get first h1 in the page
-      const h1 = window.document.getElementsByTagName("h1")[0];
-      expect(h1.innerHTML).to.equal("Hello World!");
-      // tell mocha that our testing is done
+describe('index.html', () => {
+  it('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
   })
 })
+
+// describe("index.html", () => {
+//   it("should say hello", (done) => {
+//     const index = fs.readFileSync("./src/index.html", "utf-8");
+//     jsdom.env(index, function (err, window) {
+//       // get first h1 in the page
+//       const h1 = window.document.getElementsByTagName("h1")[0];
+//       expect(h1.innerHTML).to.equal("Hello World!");
+//       // tell mocha that our testing is done
+//       done();
+//       window.close();
+//     });
+//   })
+// })
+
 
